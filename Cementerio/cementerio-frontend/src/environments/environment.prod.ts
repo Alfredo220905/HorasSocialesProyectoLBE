6619c868b8231febb5d5dbe2a9225293306bd31a
Anderson Moran <andersonalfredoescobarmoran@gmail.com>
2026-06-21 23:21:29 -0600

    cambios

--- Cementerio/cementerio-frontend/src/environments/environment.prod.ts
+++ Cementerio/cementerio-frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: '/api' // En Railway, si el backend y frontend tienen dominios diferentes, se cambia aquí.
+  apiUrl: 'https://horassocialesproyectolbe-production.up.railway.app/api' // En Railway, si el backend y frontend tienen dominios diferentes, se cambia aquí.
 };
